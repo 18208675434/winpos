a2472393f16e81783ec9d49e97f88449e0b11f61
zhushuaishuai <zhushuaishuai@zhuizhikeji.com>
2020-09-08 22:08:37 +0800

    会员中心代码合并

---
+++ ZhuiZhi_Integral_Scale_Page/MemberCenter/model/Verifysmscode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZhuiZhi_Integral_Scale_UncleFruit.MemberCenter.model
+{
+   public class Verifysmscode
+    {
+       /// <summary>
+       /// 验证码
+       /// </summary>
+        public string smscode { get; set; }
+       /// <summary>
+       /// 会员号
+       /// </summary>
+        public string memberid { get; set; }
+
+    }
+}
