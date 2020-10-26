06a33ddb86264cb078c7050604973b9a6c937f4b
zhushuaishuai <zhushuaishuai@zhuizhikeji.com>
2020-10-26 20:20:12 +0800

    充值面额接口调试完成，暂用旧接口

--- ZhuiZhi_Integral_Scale_Page/Common/HttpUtil.cs
+++ ZhuiZhi_Integral_Scale_Page/Common/HttpUtil.cs
@@ -3369,61 +3369,18 @@ namespace ZhuiZhi_Integral_Scale_UncleFruit.Common
 
 
         #region 会员中心
-        ///// <summary>
-        ///// 商户所有 充值面额设置  2020-10-26 修改接口
-        ///// </summary>
-        ///// <param name="errormsg"></param>
-        ///// <returns></returns>
-        //public List<ListAllTemplate> ListAllTemplate(ref string errormsg)
-        //{
-        //    try
-        //    {
-        //        string url = "/pos/member/balance/listalltemplate";
-
-        //        SortedDictionary<string, string> sort = new SortedDictionary<string, string>();
-
-        //        string json = HttpGET(url, sort);
-                
-        //        ResultData rd = JsonConvert.DeserializeObject<ResultData>(json);
-        //        if (rd.code == 0)
-        //        {
-        //            List<ListAllTemplate> resultobj = JsonConvert.DeserializeObject<List<ListAllTemplate>>(rd.data.ToString());
-        //            resultobj.OrderBy(e => e.amount);
-        //            return resultobj;
-        //        }
-        //        else
-        //        {
-        //            try { LogManager.WriteLog("Error", "listalltemplate:" + json); }
-        //            catch { }
-        //            errormsg = rd.message;
-        //            return null;
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        LogManager.WriteLog("Error", "获取用户信息异常：" + ex.Message);
-        //        errormsg = "网络连接异常，请检查网络连接";
-        //        return null;
-        //    }
-        //}
-
         /// <summary>
-        /// 商户所有 充值面额设置
+        /// 商户所有 充值面额设置  2020-10-26 修改接口
         /// </summary>
         /// <param name="errormsg"></param>
         /// <returns></returns>
-        public List<ListAllTemplate> ListAllTemplate(ref string errormsg,string memberid="")
+        public List<ListAllTemplate> ListAllTemplate(ref string errormsg)
         {
             try
             {
-                string url = "/pos/member/balance/listalltemplatebyshopid";
+                string url = "/pos/member/balance/listalltemplate";
 
                 SortedDictionary<string, string> sort = new SortedDictionary<string, string>();
-                sort.Add("shopid",MainModel.CurrentShopInfo.shopid);
-                if (string.IsNullOrEmpty(memberid))
-                {
-                    sort.Add("memberid",memberid);
-                }
 
                 string json = HttpGET(url, sort);
 
@@ -3449,6 +3406,49 @@ namespace ZhuiZhi_Integral_Scale_UncleFruit.Common
                 return null;
             }
         }
+
+        ///// <summary>
+        ///// 商户所有 充值面额设置
+        ///// </summary>
+        ///// <param name="errormsg"></param>
+        ///// <returns></returns>
+        //public List<ListAllTemplate> ListAllTemplate(ref string errormsg,string memberid="")
+        //{
+        //    try
+        //    {
+        //        string url = "/pos/member/balance/listalltemplatebyshopid";
+
+        //        SortedDictionary<string, string> sort = new SortedDictionary<string, string>();
+        //        sort.Add("shopid",MainModel.CurrentShopInfo.shopid);
+        //        if (string.IsNullOrEmpty(memberid))
+        //        {
+        //            sort.Add("memberid",memberid);
+        //        }
+
+        //        string json = HttpGET(url, sort);
+
+        //        ResultData rd = JsonConvert.DeserializeObject<ResultData>(json);
+        //        if (rd.code == 0)
+        //        {
+        //            List<ListAllTemplate> resultobj = JsonConvert.DeserializeObject<List<ListAllTemplate>>(rd.data.ToString());
+        //            resultobj.OrderBy(e => e.amount);
+        //            return resultobj;
+        //        }
+        //        else
+        //        {
+        //            try { LogManager.WriteLog("Error", "listalltemplate:" + json); }
+        //            catch { }
+        //            errormsg = rd.message;
+        //            return null;
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        LogManager.WriteLog("Error", "获取用户信息异常：" + ex.Message);
+        //        errormsg = "网络连接异常，请检查网络连接";
+        //        return null;
+        //    }
+        //}
 
 
         /// <summary>
