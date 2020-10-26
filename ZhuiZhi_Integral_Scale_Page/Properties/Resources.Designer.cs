0d66d35f9f650003283b04f12fb812b066116d6d
sunyanan <sunyanan@163.com>
2020-10-26 17:54:58 +0800

    批量售卡 ico

--- ZhuiZhi_Integral_Scale_Page/Properties/Resources.Designer.cs
+++ ZhuiZhi_Integral_Scale_Page/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace ZhuiZhi_Integral_Scale_UncleFruit.Properties {
         /// <summary>
         ///   查找 System.Drawing.Bitmap 类型的本地化资源。
         /// </summary>
+        internal static System.Drawing.Bitmap batchsale {
+            get {
+                object obj = ResourceManager.GetObject("batchsale", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
         internal static System.Drawing.Bitmap card {
             get {
                 object obj = ResourceManager.GetObject("card", resourceCulture);
