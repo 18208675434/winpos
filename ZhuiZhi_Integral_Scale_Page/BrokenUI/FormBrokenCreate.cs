714b967bec414ddee98e7b65c227042e41302e27
sunyanan <sunyanan@163.com>
2020-10-29 10:14:26 +0800

    修复新建保存暂存时按钮状态不正确

--- ZhuiZhi_Integral_Scale_Page/BrokenUI/FormBrokenCreate.cs
+++ ZhuiZhi_Integral_Scale_Page/BrokenUI/FormBrokenCreate.cs
@@ -102,7 +102,8 @@ namespace ZhuiZhi_Integral_Scale_UncleFruit
             btnScan.Select();
 
             CurrentProducts = DbJsonUtil.GetRecord<List<BrokenProduct>>(DbJsonUtil.BrokenProducts);
-            LoadDgvCart();
+            //LoadDgvCart();
+            RefreshCart();
         }
 
         private void frmMain_Load(object sender, EventArgs e)
@@ -1190,7 +1191,7 @@ namespace ZhuiZhi_Integral_Scale_UncleFruit
             }
         }
 
-        private void btnBatchBroken_Click(object sender, EventArgs e)
+        private void btnBatchBrokenbtnBatchBroken_Click(object sender, EventArgs e)
         {
             try
             {
